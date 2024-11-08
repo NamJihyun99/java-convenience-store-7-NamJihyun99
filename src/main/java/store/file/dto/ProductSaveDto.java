d20c3c7a587c4691e2859fc1a7588324df032d91
NamJihyun99 <namjihyun9936@gmail.com>
2024-11-08 18:01:59 +0900

    feat: 상품 필드 입력 유효성 검사

--- src/main/java/store/file/dto/ProductSaveDto.java
+++ src/main/java/store/file/dto/ProductSaveDto.java
@@ -1,19 +1,21 @@
 package store.file.dto;
 
-import store.file.exception.ExceptionCode;
-
+import java.math.BigInteger;
 import java.util.List;
+
+import static store.file.exception.ExceptionCode.DTO_NOT_MATCHED;
+import static store.file.exception.ExceptionCode.FIELD_TYPE_NOT_MATCHED;
 
 public class ProductSaveDto implements SaveDto {
 
-    private final String name;
-    private final String price;
-    private final String quantity;
-    private final String promotion;
+    public final String name;
+    public final BigInteger price;
+    public final BigInteger quantity;
+    public final String promotion;
 
     public static final int numberOfFields = 4;
 
-    private ProductSaveDto(String name, String price, String quantity, String promotion) {
+    private ProductSaveDto(String name, BigInteger price, BigInteger quantity, String promotion) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
@@ -21,13 +23,44 @@ public class ProductSaveDto implements SaveDto {
     }
 
     public static ProductSaveDto create(List<String> params) {
+        validateParams(params);
+        String name = params.get(0);
+        BigInteger price = new BigInteger(params.get(1));
+        BigInteger quantity = new BigInteger(params.get(2));
+        String promotion = params.get(3);
+        if (params.get(3).equals("null")) {
+            promotion = null;
+        }
+        return new ProductSaveDto(name, price, quantity, promotion);
+    }
+
+    private static void validateParams(List<String> params) {
         validateSize(params);
-        return new ProductSaveDto(params.get(0), params.get(1), params.get(2), params.get(3));
+        validateString(params.get(0));
+        validateNumber(params.get(1));
+        validateNumber(params.get(2));
+        validateString(params.get(3));
+    }
+
+    private static void validateNumber(String number) {
+        try {
+            if (number == null || new BigInteger(number).compareTo(BigInteger.ZERO) < 0) {
+                throw new IllegalArgumentException(FIELD_TYPE_NOT_MATCHED.message);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(FIELD_TYPE_NOT_MATCHED.message);
+        }
+    }
+
+    private static void validateString(String stringInput) {
+        if (stringInput == null || stringInput.isBlank()) {
+            throw new IllegalArgumentException(FIELD_TYPE_NOT_MATCHED.message);
+        }
     }
 
     private static void validateSize (List<String> params) {
         if (params.size() != numberOfFields) {
-            throw new IllegalArgumentException(ExceptionCode.DTO_NOT_MATCHED.message);
+            throw new IllegalArgumentException(DTO_NOT_MATCHED.message);
         }
     }
 
