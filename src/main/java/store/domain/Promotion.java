3f7df8b836483d4224825c321cdf0906ea836c9f
NamJihyun99 <namjihyun9936@gmail.com>
2024-11-09 17:33:57 +0900

    rename: rename Promotion to BuyNGetOneFree

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,49 +1,7 @@
 package store.domain;
 
-import java.math.BigInteger;
 import java.time.LocalDate;
 
-import static store.common.ExceptionCode.*;
+public interface Promotion {
 
-public class Promotion {
-
-    private final String name;
-    private final BigInteger get;
-    private final BigInteger buy;
-    private final LocalDate startDate;
-    private final LocalDate endDate;
-
-
-    private Promotion(String name, BigInteger get, BigInteger buy, LocalDate startDate, LocalDate endDate) {
-        this.name = name;
-        this.get = get;
-        this.buy = buy;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
-
-    public static Promotion create(String name, BigInteger get, BigInteger buy, LocalDate startDate, LocalDate endDate) {
-        validateName(name);
-        validateRule(get, buy);
-        validateDate(startDate, endDate);
-        return new Promotion(name, get, buy, startDate, endDate);
-    }
-
-    private static void validateName(String name) {
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException(NAME_BLANK.message);
-        }
-    }
-
-    private static void validateRule(BigInteger get, BigInteger buy) {
-        if (get == null || buy == null || get.compareTo(BigInteger.ZERO) <= 0 || buy.compareTo(BigInteger.ZERO) <= 0) {
-            throw new IllegalArgumentException(NUMBER_NEGATIVE.message);
-        }
-    }
-
-    private static void validateDate(LocalDate startDate, LocalDate endDate) {
-        if (startDate == null || endDate == null || startDate.isAfter(endDate)) {
-            throw new IllegalArgumentException(PERIOD_NOT_MATCHED.message);
-        }
-    }
 }
--- src/test/java/store/domain/BuyNGetOneFreeTest.java
+++ src/test/java/store/domain/BuyNGetOneFreeTest.java
@@ -9,22 +9,22 @@ import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.*;
 
-class PromotionTest {
+class BuyNGetOneFreeTest {
 
     @DisplayName("프로모션 객체 생성 성공")
     @Test
     void create_success() {
-        Promotion promotion = Promotion.create("탄산2+1",
+        BuyNGetOneFree buyNGetOneFree = BuyNGetOneFree.create("탄산2+1",
                 BigInteger.TWO, BigInteger.ONE,
                 LocalDate.parse("2024-01-01"), LocalDate.parse("2024-12-31"));
-        assertThat(promotion).isInstanceOf(Promotion.class);
+        assertThat(buyNGetOneFree).isInstanceOf(BuyNGetOneFree.class);
 
     }
 
     @DisplayName("프로모션 이름이 공백이면 예외가 발생한다.")
     @Test
     void create_name_blank() {
-        assertThatThrownBy(() -> Promotion.create("",
+        assertThatThrownBy(() -> BuyNGetOneFree.create("",
                 BigInteger.TWO, BigInteger.ONE,
                 LocalDate.parse("2024-01-01"), LocalDate.parse("2024-12-31")))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -34,7 +34,7 @@ class PromotionTest {
     @DisplayName("프로모션 적용 상품 개수가 0보다 작거나 같으면 예외가 발생한다.")
     @Test
     void create_number_negative() {
-        assertThatThrownBy(() -> Promotion.create("탄산2+1",
+        assertThatThrownBy(() -> BuyNGetOneFree.create("탄산2+1",
                 BigInteger.TWO, BigInteger.ZERO,
                 LocalDate.parse("2024-01-01"), LocalDate.parse("2024-12-31")))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -44,7 +44,7 @@ class PromotionTest {
     @DisplayName("프로모션 종료날짜가 시작날짜보다 앞서면 예외가 발생한다.")
     @Test
     void create_period_not_matched() {
-        assertThatThrownBy(() -> Promotion.create("탄산2+1",
+        assertThatThrownBy(() -> BuyNGetOneFree.create("탄산2+1",
                 BigInteger.TWO, BigInteger.ONE,
                 LocalDate.parse("2025-01-01"), LocalDate.parse("2024-12-31")))
                 .isInstanceOf(IllegalArgumentException.class)
